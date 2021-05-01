10d56b5c9ea56a15ccc48e0087b566df590662bf
TonyBi <tonnybida@gmail.com>
2021-05-01 16:31:04 +1200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
+
 
 function App() {
   return (
@@ -9,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="primary">Go to Generator</Button>
       </header>
     </div>
   );
